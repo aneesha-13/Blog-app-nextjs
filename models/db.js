199decde5c4f66aa878550e5a3f98d422866518d
aneesha-13 <aneesha.constella@gmail.com>
2022-02-09 14:00:01 +0530

    project and model created

---
+++ models/db.js
@@ -0,0 +1,18 @@
+const mongoose=require(mongoose);
+
+const blogSchema=new.mongoose.Schema({
+    authorid:{
+        type: String
+    },
+    author:{
+        type: String
+    },
+    title:{
+        type: String
+    },
+    content:{
+        type: String
+    }
+
+})
+module.exports=mongoose.models.blog || mongoose.model('blog',blogSchema);
